5237e2a1b6a8f1e162cfa872e694de77a26c05d5
ericroca-dev <ericroca.dev@gmail.com>
2019-05-30 10:13:23 +0300

    Initial Commit

---
+++ Hotel Reservation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hotel Reservation
+//
+//  Created by Eric Roca on 30/05/2019.
+//  Copyright © 2019 Eric Roca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Hotel ReservationTests/Hotel_ReservationTests.swift
@@ -0,0 +1,34 @@
+//
+//  Hotel_ReservationTests.swift
+//  Hotel ReservationTests
+//
+//  Created by Eric Roca on 30/05/2019.
+//  Copyright © 2019 Eric Roca. All rights reserved.
+//
+
+import XCTest
+@testable import Hotel_Reservation
+
+class Hotel_ReservationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
